7b66c39e46504a2e858d1c8c4f251561c7176d7a
Rory Garand <github@rorygarand.com>
2023-10-14 17:46:57 -0400

    first

---
+++ README.md
@@ -0,0 +1,30 @@
+# Erin Garand's Site
+
+## Installation
+
+```bash
+$ yarn
+
+```
+
+
+## Developing
+
+Start a development server:
+
+```bash
+yarn dev
+
+# or start the server and open the app in a new browser tab
+yarn dev -- --open
+```
+
+
+## Building
+
+To create a production version of your app:
+
+```bash
+yarn build
+```
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<style>
+	.box {
+		backdrop-filter: sepia(100%);
+		border-radius: 64px;
+		flex-direction: column;
+		max-height: 550px;
+		padding: 80px;
+	}
+
+	.center {
+		align-items: center;
+		justify-content: center;
+	}
+
+	.hero {
+		background: var(--gradient);
+		height: 100%;
+		transition: background .2s linear;
+		width: 100%;
+	}
+</style>
+
+<section class="hero center">
+	<div class="box center">
+		<h1>Hello, this is Erin's site</h1>
+		<h4>I have 3 dogs and 11 cats</h4>
+	</div>
+</section>
